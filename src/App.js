69d568b0e9bb42b73864d5ada83412d2fe527efc
ZSwensrude <97464094+ZSwensrude@users.noreply.github.com>
2024-01-25 13:06:24 -0700

    now gets monsters from site

--- src/App.js
+++ src/App.js
@@ -1,34 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
 import { CardList } from './components/MonsterList/list.component';
+import { useState, useEffect } from 'react';
 
 function App() {
-  const monsters = [
-    {
-      "id": 1,
-      "name": "Leanne Graham",
-      "username": "Bret",
-      "email": "Sincere@april.biz",
-      "address": {
-        "street": "Kulas Light",
-        "suite": "Apt. 556",
-        "city": "Gwenborough",
-        "zipcode": "92998-3874",
-        "geo": {
-        "lat": "-37.3159",
-        "lng": "81.1496"
-        }
-      },
-      "phone": "1-770-736-8031 x56442",
-      "website": "hildegard.org",
-      "company": {
-        "name": "Romaguera-Crona",
-        "catchPhrase": "Multi-layered client-server neural-net",
-        "bs": "harness real-time e-markets"
-      }
+  const [monsters, setMonsters] = useState([]);
+
+  useEffect( () => {
+    const fetchMonsters = async () => {
+      const response = await fetch("https://jsonplaceholder.typicode.com/users");
+      const monsters = await response.json();
+      console.log(monsters);
+      setMonsters(monsters);
     }
-  ];
-    
+    fetchMonsters();
+  }, []);
+
 
   return (
     <div className="App">
