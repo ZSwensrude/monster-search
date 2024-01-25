04d22a55e155d57ccecb7f6884f0f68100fa32fc
ZSwensrude <97464094+ZSwensrude@users.noreply.github.com>
2024-01-25 12:38:35 -0700

    get base functionality in, dummy data

--- src/components/MonsterList/list.component.jsx
+++ src/components/MonsterList/list.component.jsx
@@ -1,16 +1,13 @@
 import React from "react";
-import { Card } from '../MonsterCard/card.component'
+import Card from "../MonsterCard/card.component";
 
 import './list.styles.css';
 
-const CardList = ({ monsters }) => {
-  return (
-    <div className="cardlist">
-      {monsters.map(monster => {
-        <Card key={monster.id} monster={monster} />
-      })}
-    </div>
-  );
-};
-
-export default CardList;
+export const CardList = ({ monsters }) => (
+  <div className="cardlist">
+  {monsters.map(monster => (
+    <Card key={monster.id} monster={monster} />
+    ))}
+  </div>
+);
+  
